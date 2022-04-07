2ceeb2a3725441951e9324f6063f0c19b523a6d7
Del83 <91467158+Del83@users.noreply.github.com>
2022-04-07 09:47:08 +0200

    Premier commit

---
+++ scripts/index.js
@@ -0,0 +1,133 @@
+import { recipes } from "../data/recipes.js";
+import { recipeTemplate } from "./recipe_template.js";
+
+/* ---------- Eléments du DOM ---------- */
+
+/* ---------- Creation des tableaux ---------- */
+let recipesList = [];
+let ingredientstList = [];
+let appliancesList = [];
+let ustensilsList = [];
+
+/* ---------- Classe RECIPE ---------- */
+class Recipe {
+  constructor({
+    id,
+    name,
+    servings,
+    ingredients,
+    time,
+    description,
+    appliance,
+    ustensils,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.parts = servings;
+    this.ingredients = ingredients;
+    this.time = time;
+    this.description = description;
+    this.appliance = appliance;
+    this.ustensils = ustensils;
+    this.visible = true; /// pour la fonction de maj des listes d'items
+  }
+
+  createRecipeCard() {
+    this.recipeCard = document.createElement("article");
+    this.recipeCard.classList.add("recipe-item");
+    this.recipeCard.classList.add(this.id);
+    this.recipeCard.setAttribute("id", this.id);
+    this.recipeCard.innerHTML = recipeTemplate;
+    // HTML
+    this.recipeCard.querySelector(".recipe-title").innerText = this.name;
+    this.recipeCard.querySelector(".recipe-time").innerText =
+      this.time + " min";
+    const recipeIngredients = this.recipeCard.querySelector(
+      ".recipe-ingredients"
+    );
+    const ingredients = this.ingredients;
+    ingredients.forEach((ingredient) => {
+      const ingredientDetail = document.createElement("li");
+      ingredientDetail.classList.add("recipe-ingredients-detail");
+      const ingredientName = document.createElement("span");
+      ingredientName.classList.add("recipe-ingredients-detail-name");
+      ingredientName.setAttribute("data-ingredient", ingredient.ingredient);
+      ingredientName.innerText = ingredient.ingredient;
+      ingredientDetail.appendChild(ingredientName);
+      // Adapter l'affichage aux différences de data (quantity/quantite, unit ou non, ponctuation inadéquate)
+      if (ingredient.quantite || ingredient.quantity) {
+        const quantiteIngredient = document.createElement("span");
+        quantiteIngredient.classList.add("recipe-ingredients-detail-quantity");
+        let quantite = "";
+        if (ingredient.quantite) {
+          quantite = ingredient.quantite;
+        } else {
+          quantite = ingredient.quantity;
+        }
+        if (ingredient.unit) {
+          quantiteIngredient.innerText =
+            ": " + quantite + " " + ingredient.unit;
+        } else {
+          quantiteIngredient.innerText = ": " + quantite;
+        }
+        let quantiteIngredientCorrection =
+          quantiteIngredient.textContent.replace(".", ",");
+        quantiteIngredient.innerText = quantiteIngredientCorrection;
+        ingredientDetail.appendChild(quantiteIngredient);
+      }
+      recipeIngredients.appendChild(ingredientDetail);
+    });
+    this.recipeCard.querySelector(".recipe-description").innerText =
+      this.ellipsis();
+
+    //   const createIngredientsList = () => {
+    //     const ingredients = this.ingredients;
+    //     console.log(toLowerCase(ingredients));
+    //     //const ingredientsmini = ingredients.toLowerCase();
+    //     ingredients.forEach((ingredient) => {
+    //       ingredientstList.push(ingredient.ingredient);
+    //       console.log(ingredientstList);
+    //       /*if (ingredient.ingredient === ingredient.ingredient) {
+    //         return ingredientstList.toggle(ingredient.ingredient);
+    //       }*/
+    //     });
+    //   };
+    //   createIngredientsList();
+    //   return this.recipeCard;
+    // }
+  }
+  ellipsis() {
+    ///Récupération du texte
+    let text = this.description;
+    ///On remplace les suites d'espaces par un seul espace
+    text = text.replace(/  +/g, " ");
+    /// si le texte est plus long que 210 caractères...
+    if (text.length > 210) {
+      // On coupe les X premiers caractères...
+      text = text.substr(0, 210);
+      //On coupe à nouveau pour enlever le dernier mot si il a été coupé en 2...
+      text = text.substr(0, Math.min(text.length, text.lastIndexOf(" ")));
+      //On retourne le texte coupé avec les 3 points à la fin
+      return text + " ...";
+    } else {
+      // ou le texte si inférieur à 210 caractères
+      return text;
+    }
+  }
+}
+
+const createRecipes = () => {
+  const domSectionResult = document.getElementById("result-section");
+  recipes.forEach((recipe) => {
+    recipesList.push(new Recipe(recipe));
+  });
+  /// Insertion des éléments des cartes de recettes
+  recipesList.forEach((recipe) => {
+    domSectionResult.appendChild(recipe.createRecipeCard());
+  });
+};
+
+const init = () => {
+  createRecipes();
+};
+init();
---
+++ scripts/recipe_template.js
@@ -0,0 +1,21 @@
+export const recipeTemplate = `
+<div class="recipe-thumbnail">
+</div>
+<div class="recipe-details">
+    <div class="recipe-details-header">
+        <h2 class="recipe-title"></h2>
+        <div class="recipe-time-container">
+            <img class="recipe-time-icon"
+                src="./assets/clock.svg" alt="icone horloge" />
+            <span class="recipe-time">
+            </span>
+        </div>
+    </div>
+    <div class="recipe-details-main">
+        <ul class="recipe-ingredients">
+        </ul>
+        <p class="recipe-description">
+        </p>
+    </div>
+</div>
+`;
